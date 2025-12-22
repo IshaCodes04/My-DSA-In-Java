cdf1aab6d3c600c08955411e5b43858454d88cf7
IshaCodes04 <ishu010.com@gmail.com>
2025-12-22 21:13:54 +0530

    outputs in java

---
+++ output in java/javabasics.java
@@ -0,0 +1,8 @@
+public class javabasics {
+  public static void main(String args[]){
+     System.out.print("Hello Isha!"); // print as it is
+     System.out.print("Hello world\n"); // line ka space
+     System.out.println("Hello sinchan!");
+     System.out.println("Hello java!");
+  }
+}
