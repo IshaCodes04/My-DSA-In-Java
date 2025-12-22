bde5f94665434970350ed93e28a47ae967d15c40
IshaCodes04 <ishu010.com@gmail.com>
2025-12-22 20:27:53 +0530

    boilerplate code in java

---
+++ Boilerplate code in java/javabasics.java
@@ -0,0 +1,10 @@
+// So this was the whole boilerplate code in java which is must to wrote in evry file
+
+// First we define a class
+public class javabasics {
+  // now we define a function in class
+  public static void main(String args[]) {
+       
+  }
+
+}
