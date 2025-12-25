ae0462d1bc4a143c01a4657b4a5dfcf41efa2327
IshaCodes04 <ishu010.com@gmail.com>
2025-12-25 19:50:51 +0530

    define variables

--- variables in java/javabasics.java
+++ variables in java/javabasics.java
@@ -1,1 +1,20 @@
+// define a variable
 
+public class javabasics {
+
+  public static void main(String args[]) {
+    int a = 10;
+    int b = 5;
+    System.out.println(a);
+    System.out.println(b);
+    String name = "isha singh";
+    System.out.println(name);
+
+
+    a = 50; // we also changes var values
+    System.out.println(a);
+
+    a = b ; // here we assign value of b in a
+    System.out.println(a);
+  }
+}
