7780161883d1d3730edcaad1d10517f4c15b829d
IshaCodes04 <ishu010.com@gmail.com>
2025-12-25 20:16:18 +0530

    define data type

--- data types in java/javabasics.java
+++ data types in java/javabasics.java
@@ -1,3 +1,17 @@
+// define a data type
 
+public class javabasics{
+
+  public static void main(String args[]){
+    byte  b = 9; // byte data type
+    System.out.println(b);
+
+    char ch = 'a'; // char data type
+    System.out.println(ch);
+
+    
+
+  }
+}
 
 
