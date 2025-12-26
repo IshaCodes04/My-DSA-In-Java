44e4ce8e598b6d8b263f3b4ade9925690cc44820
IshaCodes04 <ishu010.com@gmail.com>
2025-12-26 20:17:22 +0530

    nextInt

--- Inputs in java/javabasics.java
+++ Inputs in java/javabasics.java
@@ -4,13 +4,16 @@ import java.util.*; // util is a java package
 
 public class javabasics {
    public static void main(String args[]){
+
      Scanner sc  = new Scanner(System.in);
-     String input = sc.next(); // sc.next for take input and store it in String name input
-     System.out.println(input);
 
-     String name = sc.nextLine(); // nextLine used for count spaces like isha singh full name ayega ab input m with space
-     System.out.println(name);
+    // String input = sc.next(); // sc.next for take input and store it in String name input
+     //System.out.println(input);
 
+     //String name = sc.nextLine(); // nextLine used for count spaces like isha singh full name ayega ab input m with space as well as a sentence or paragraphs
+     //System.out.println(name);
 
+     int number = sc.nextInt(); // used for take integer inputs
+     System.out.println(number);
    }
 }
