51b430243c1b3f7ceda31f69a5ba94f9ad578c58
IshaCodes04 <ishu010.com@gmail.com>
2025-12-26 19:41:57 +0530

    String input

--- Inputs in java/javabasics.java
+++ Inputs in java/javabasics.java
@@ -1,8 +1,12 @@
 // take inputs in java
 
-import java.util.*;
+import java.util.*; // util is a java package
+
 public class javabasics {
    public static void main(String args[]){
      Scanner sc  = new Scanner(System.in);
+     String input = sc.next(); // sc.next for take input and store it in String name input
+     System.out.println(input);
+
    }
 }
--- Inputs in java/readme.md
+++ Inputs in java/readme.md
@@ -1,4 +1,5 @@
 *Inputs in java*
 
 - user se input kaise lenge java mein
-- java m input lene ke liye class bnate h scanner ki 
+- java m input lene ke liye class bnate h scanner ki
+- Scanner object used different functions and captured diff data types and store in variables. 
