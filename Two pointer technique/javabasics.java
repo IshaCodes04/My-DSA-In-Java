931a20785f11f24c6557da3f83e59d0bf52a9394
IshaCodes04 <ishu010.com@gmail.com>
2026-01-27 14:52:34 +0530

    sorted or unsorted array

--- Two pointer technique/javabasics.java
+++ Two pointer technique/javabasics.java
@@ -1,5 +1,9 @@
 // Ques :-  An Unsorted array , we need to 2 numbers whose sum is equal to given target and don't use exta space? In this ques 2 pointer was used.
 
+// [2,5,1,7,3] unsorted array index
+
+// [1,2,3,5,7] sorted array , after sorting is allowed becos in ques return pair of numbers demanded
+
 public class javabasics {
     
 }
