f5b899beb22e0e5691f2e94352f5d196ceceba8a
IshaCodes04 <ishu010.com@gmail.com>
2026-01-27 15:20:54 +0530

    iteration position

--- Two pointer technique/javabasics.java
+++ Two pointer technique/javabasics.java
@@ -3,6 +3,10 @@
 // [2,5,1,7,3] unsorted array index
 
 // [1,2,3,5,7] sorted array , after sorting is allowed becos in ques return pair of numbers demanded
+  
+// i + j = 1+7 = 8 , here 8 is not equal to the target sum then return nhi krenge here we need to change iteration position of i or j acc to the 3 possibilites :-
+
+// find target sum = 6 , output = 8 , if 8 == 6 then return kr denge , if 8<6 then change iteration postion , if 8>6 then also changed iteration position of i and j 
 
 public class javabasics {
     
