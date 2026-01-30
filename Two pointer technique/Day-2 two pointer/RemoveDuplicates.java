a91c26a1b4bfda1d83fa6b201f4c33fba5c56869
IshaCodes04 <ishu010.com@gmail.com>
2026-01-30 19:59:12 +0530

    given in ques

---
+++ Two pointer technique/Day-2 two pointer/RemoveDuplicates.java
@@ -0,0 +1,8 @@
+// Leetcode ques 26. Remove Duplicates from sorted array.
+
+// Given :- ek integer array nums jo sorted h non-decreasing order mein for ex - [1,1,2,2,3,4,4,4,5]
+ 
+
+public class RemoveDuplicates {
+  
+}
