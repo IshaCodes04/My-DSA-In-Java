91d98b9a96d8f807940102a68664e37719c49571
IshaCodes04 <ishu010.com@gmail.com>
2026-01-30 20:13:54 +0530

    task

--- Two pointer technique/Day-2 two pointer/RemoveDuplicates.java
+++ Two pointer technique/Day-2 two pointer/RemoveDuplicates.java
@@ -1,7 +1,12 @@
 // Leetcode ques 26. Remove Duplicates from sorted array.
 
-// Given :- ek integer array nums jo sorted h non-decreasing order mein for ex - [1,1,2,2,3,4,4,4,5]
- 
+// Given :- Ek integer array nums jo sorted h non-decreasing order mein for ex - [1,1,2,2,3,4,4,4,5]
+
+// Task :- Remove duplicates In-Place - means we don't create any extra array, every changes occurs in same array.
+
+// Har unique element sirf ek br he ayega 
+
+// Relative order same rhega - mean jo order pehle tha whi rhega
 
 public class RemoveDuplicates {
   
